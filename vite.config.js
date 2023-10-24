ec85efc0e59d529be4e2fd1681112ded8f51319b
hipig <205270006@qq.com>
2023-10-24 23:31:36 +0800

    优化前端打包

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,22 @@ export default defineConfig({
             '@': '/resources/ts',
         },
     },
+    build: {
+        reportCompressedSize: false,
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                }
+            }
+        }
+    }
+
 });
