9f7c3397e82d7c9eb2edf9e02b2884745e86d7bd
hipig <205270006@qq.com>
2023-10-24 21:21:30 +0800

    优化前端路由

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -4,7 +4,7 @@ import appRoutes from './routes';
 import createRouteGuard from './guard';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory('/'),
     routes: [
         {
             path: '/',
