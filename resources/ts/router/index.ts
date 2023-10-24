5eb3d275ef5000c8bec13b920e842d1f3cc356d7
hipig <205270006@qq.com>
2023-10-24 22:38:07 +0800

    优化添加会话

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -8,7 +8,7 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: 'chat',
+            redirect: '/chat',
         },
         ...appRoutes,
     ],
