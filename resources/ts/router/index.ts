3efed1f5029198d40a06bee750054c02ee1ff269
hipig <205270006@qq.com>
2023-10-24 22:42:06 +0800

    优化添加会话

--- resources/ts/router/index.ts
+++ resources/ts/router/index.ts
@@ -3,12 +3,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import appRoutes from './routes';
 import createRouteGuard from './guard';
 
+export const MAIN_LAYOUT = () => import('@/layout/MainLayout.vue');
+
 const router = createRouter({
     history: createWebHistory('/'),
     routes: [
         {
             path: '/',
             redirect: '/chat',
+            component: MAIN_LAYOUT
         },
         ...appRoutes,
     ],
