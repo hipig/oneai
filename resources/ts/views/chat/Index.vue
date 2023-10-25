80e69688ef3b22071e54649019754f92750f3339
hipig <205270006@qq.com>
2023-10-25 23:40:31 +0800

    优化消息列表

--- resources/ts/views/chat/Index.vue
+++ resources/ts/views/chat/Index.vue
@@ -4,7 +4,18 @@
         <div class="flex flex-auto flex-col overflow-hidden">
             <simplebar ref="scrollRef" class="h-full">
                 <div class="w-full max-w-screen-lg mx-auto px-4 lg:px-6">
-                    <div class="message-list w-full flex flex-col-reverse py-2 sm:py-6">
+                    <div v-if="!messageList" class="animate-pulse w-full flex flex-col-reverse py-2 sm:py-6">
+                        <div v-for="i in 8" class="w-full message-item py-3 flex flex-col sm:flex-row items-start sm:space-x-2 space-y-1 sm:space-y-0">
+                            <div class="flex md:flex-none">
+                                <div class="w-8 h-8 p-px bg-gray-200 border border-gray-200 rounded-full overflow-hidden"></div>
+                            </div>
+                            <div class="flex-auto py-0.5 space-y-1">
+                                <div class="w-64 h-3 bg-gray-200 rounded"></div>
+                                <div class="w-full h-3 bg-gray-200 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div v-else class="message-list w-full flex flex-col-reverse py-2 sm:py-6">
                         <MessageItem v-for="message in messageList" :type="message.type" :content="message.content" :loading="message?.loading" />
                     </div>
                 </div>
