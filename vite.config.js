42dc0733d2eac484f0276b4b721782acdef83e39
hipig <205270006@qq.com>
2023-10-25 22:50:47 +0800

    优化前端打包

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
     plugins: [
@@ -16,6 +17,7 @@ export default defineConfig({
                 },
             },
         }),
+        viteCompression()
     ],
     resolve: {
         alias: {
